aad5a82731331105cc9cfe9b8d6bc4635640ee69
Mobeen Ahmad <Mobeen0096@gmail.com>
2024-10-22 18:06:28 +0500

    Changes

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 import type { MetaFunction } from "@remix-run/node";
 import { Box, Typography, Container } from "@mui/material";
-import FolderStructure from "../../public/assets/image.png";
 import { Link } from "@remix-run/react";
 export const meta: MetaFunction = () => {
   return [
@@ -20,19 +19,6 @@ export default function Index() {
               Welcome to <span className="sr-only">Remix BoilerPlate</span>
             </h1>
           </header>
-          <nav className="">
-            <p className="">What&apos;s next?</p>
-            <ul>
-              {resources.map(({ href, text, icon }) => (
-                <li key={href}>
-                  <a className="" href={href} target="_blank" rel="noreferrer">
-                    {icon}
-                    {text}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </nav>
         </div>
         <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
           {" "}
@@ -87,13 +73,12 @@ export default function Index() {
         >
           Folder Structure
         </Typography>
-        <img src={FolderStructure} alt="" />
       </Box>
 
-      <Container
+      <Box
         maxWidth="100%"
         sx={{
-          padding: "20px 0",
+          padding: "0px 20px",
         }}
       >
         <Typography variant="h4" gutterBottom>
@@ -180,52 +165,7 @@ export default function Index() {
         <Typography paragraph>
           The lock file for Yarn to ensure consistent installations.
         </Typography>
-      </Container>
+      </Box>
     </>
   );
 }
-
-const resources = [
-  {
-    href: "https://github.com/falconitconsultant",
-    text: "Github Account",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="20"
-        viewBox="0 0 20 20"
-        fill="none"
-        className="stroke-gray-600 group-hover:stroke-current dark:stroke-gray-300"
-      >
-        <path
-          d="M8.51851 12.0741L7.92592 18L15.6296 9.7037L11.4815 7.33333L12.0741 2L4.37036 10.2963L8.51851 12.0741Z"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
-    ),
-  },
-
-  {
-    href: "https://remix.run/docs",
-    text: "Remix Docs",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="20"
-        viewBox="0 0 20 20"
-        fill="none"
-        className="stroke-gray-600 group-hover:stroke-current dark:stroke-gray-300"
-      >
-        <path
-          d="M9.99981 10.0751V9.99992M17.4688 17.4688C15.889 19.0485 11.2645 16.9853 7.13958 12.8604C3.01467 8.73546 0.951405 4.11091 2.53116 2.53116C4.11091 0.951405 8.73546 3.01467 12.8604 7.13958C16.9853 11.2645 19.0485 15.889 17.4688 17.4688ZM2.53132 17.4688C0.951566 15.8891 3.01483 11.2645 7.13974 7.13963C11.2647 3.01471 15.8892 0.951453 17.469 2.53121C19.0487 4.11096 16.9854 8.73551 12.8605 12.8604C8.73562 16.9853 4.11107 19.0486 2.53132 17.4688Z"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-        />
-      </svg>
-    ),
-  },
-];
